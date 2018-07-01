6267483b9067d40b12a50cea241ebafc97caadd7
ramhejazi <htmlpack@gmail.com>
2018-07-01 06:57:54 +0430

    add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "console": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            1,
+            "single"
+        ]
+    }
+};
